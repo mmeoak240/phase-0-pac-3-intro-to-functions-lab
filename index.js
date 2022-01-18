196b6a7f04edc41520b3864a61db8133b0969e74
Michael Meoak <mmeoak240@gmail.com>
2022-01-17 18:42:00 -0700

    commit tree d03226abfaf516c140436f1db377a2143fa54f5c

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+const speak = "I can't hear you!"
+
+function sayHiToHeadphonedRoommate(speak) {
+  if (speak === "I can\'t hear you!") {
+  return "I can\'t hear you!"}
+} else {return "YES INDEED!"}
+
